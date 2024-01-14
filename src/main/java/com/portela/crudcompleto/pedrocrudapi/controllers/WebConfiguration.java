036cda29e830af0b16ea8842529514f11931ff57
Moisés <msportela2@hotmail.com>
2024-01-14 15:31:52 -0300

    ajuste politica cors

--- src/main/java/com/portela/crudcompleto/pedrocrudapi/controllers/WebConfiguration.java
+++ src/main/java/com/portela/crudcompleto/pedrocrudapi/controllers/WebConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class WebConfiguration {
 	@Bean
@@ -13,11 +12,7 @@ public class WebConfiguration {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry
-                .addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowedMethods("*");
+				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
 			}
 		};
 	}
